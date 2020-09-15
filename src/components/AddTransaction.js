8c47c8fb0b7289e83ee2e382ba3d9590269de5bd
Justin <barreto5288@gmail.com>
2020-09-15 01:00:18 -0400

    updated placeholder text color

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -23,7 +23,7 @@ export const AddTransaction = () => {
 
   return (
     <>
-      <h3>Add new transaction</h3>
+      <h3>Add New Transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Transaction</label>
